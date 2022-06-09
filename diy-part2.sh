07a09955f17c0200949ff8d1b56f7a7c4bb6125f
hlj <ujmyvq1582@gmail.com>
2022-06-09 16:01:31 +0800

    KERNEL_PATCHVER:=5.10

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,5 +13,4 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/bootstrap/material/g' package/lean/default-settings/files/zzz-default-settings
-mkdir -p files/etc/config
-echo  0xDEADBEEF > files/etc/config/google_fu_mode
+sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.10/g' target/linux/x86/Makefile
