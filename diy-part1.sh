8d8e69be50dedaba741400ef0a0cda83117648b6
ujmyvq <1928214430@qq.com>
2023-05-03 10:03:46 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,5 +17,10 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-git clone https://github.com/KFERMercer/luci-app-tcpdump.git ./package/luci-app-tcpdump
+#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package.git;main' feeds.conf.default
+#sed -i '$a src-git diy1 https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
+#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
 echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
